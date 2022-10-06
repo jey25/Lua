3a57740651c5551fd26950b2970c5b45a71e0ee5
jey25 <jjangeoyoung@gmail.com>
2022-10-06 17:46:29 +0900

    Update yellot.lua

--- prog_lua/yellot.lua
+++ prog_lua/yellot.lua
@@ -1,17 +1,23 @@
 
-local a, b, c = 11, 22, 33
-local x,y,z = a+b, a-b, a*b
-print(x, y, z)
+-- local a, b, c = 11, 22, 33
+-- local x,y,z = a+b, a-b, a*b
+-- print(x, y, z)
 
-print(script.Parent.Parent.Parent)
+-- print(script.Parent.Parent.Parent)
 
 
 -- while 반복문
 
-count = 0
+-- count = 0
 
-while count < 10 do
-  print("hi")
-  print(count)
-  count = count + 1
+-- while count < 10 do
+--   print("hi")
+--   print(count)
+--   count = count + 1
+-- end
+
+
+function first()
+    number = 20 + 40
+    print(number)
 end
