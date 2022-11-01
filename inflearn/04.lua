fe4902f63fcc7c58fc53bb1edd3eef52dcee6ccb
jey25 <jjangeoyoung@gmail.com>
2022-11-01 22:55:52 +0900

    Create 04.lua

---
+++ inflearn/04.lua
@@ -0,0 +1,15 @@
+--스크립트 기반으로 블럭 요소 찾기
+
+--local blockPart = game.Workspace.Cube
+local blockPart = script.Parent
+
+
+--fire 를 찾아서 컬러 속성 변경
+local fire = script.Parent.Fire
+fire.Color = Color3.fromRGB(15, 178, 45)
+
+
+--컬러 변경하는 2가지 방법
+--blockPart.BrickColor = BrickColor.new("Mint")
+
+blockPart.Color = Color3.fromRGB(0, 156, 187)
