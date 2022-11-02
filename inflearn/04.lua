19d7b0a5d6c6693a18f1f7da12daea35f4f406a7
jey25 <jjangeoyoung@gmail.com>
2022-11-03 01:04:14 +0900

    Update 04.lua

--- inflearn/04.lua
+++ inflearn/04.lua
@@ -14,3 +14,25 @@ fire.Color = Color3.fromRGB(15, 178, 45)
 
 blockPart.BrickColor = BrickColor.new("Mint")
 blockPart.Color = Color3.fromRGB(0, 156, 187)
+
+
+
+-- 반복문을 통한 블록 컬러 반복 변경
+
+local colorBlock = script.Parent
+
+local red = Color3.fromRGB(255, 0, 0)
+local green = Color3.fromRGB(0, 255, 0)
+local blue = Color3.fromRGB(0, 0, 255)
+
+
+
+for count = 1, 10, 1 do
+    print("count = " .. count)
+    colorBlock.Color = red
+    wait(1)
+    colorBlock.Color = green
+    wait(1)
+    colorBlock.Color = blue
+    wait(1)
+end
