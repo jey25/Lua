2512ecd6817f8a9a7bddcd1c94859215447cbe38
jey25 <jjangeoyoung@gmail.com>
2023-01-03 00:55:53 +0900

    Update ex01.lua

--- practice/ex01.lua
+++ practice/ex01.lua
@@ -357,13 +357,65 @@ box.Touched:Connect(onTouched)
 local services = {}
 
 for _, service in ipairs(game:GetChildren()) do
- local success, result = pcall(function()
-  table.insert(services, service.Name)
- end)
+    local success, result = pcall(function()
+        table.insert(services, service.Name)
+    end)
 end
 
 table.sort(services)
 
 for _, service in ipairs(services) do
- print(service)
+    print(service)
 end
+
+
+
+-- 플레이어 충돌 없애주는 스크립트
+
+local PhysicsService = game:GetService("PhysicsService")
+local Players = game:GetService("Players")
+
+local playerCollisionGroupName = "Players"
+PhysicsService:CreateCollisionGroup(playerCollisionGroupName)
+PhysicsService:CollisionGroupSetCollidable(playerCollisionGroupName, playerCollisionGroupName, false)
+
+local previousCollisionGroups = {}
+
+local function setCollisionGroup(object)
+    if object:IsA("BasePart") then
+        previousCollisionGroups[object] = object.CollisionGroupId
+        PhysicsService:SetPartCollisionGroup(object, playerCollisionGroupName)
+    end
+end
+
+local function setCollisionGroupRecursive(object)
+    setCollisionGroup(object)
+
+    for _, child in ipairs(object:GetChildren()) do
+        setCollisionGroupRecursive(child)
+    end
+end
+
+local function resetCollisionGroup(object)
+    local previousCollisionGroupId = previousCollisionGroups[object]
+    if not previousCollisionGroupId then return end
+
+    local previousCollisionGroupName = PhysicsService:GetCollisionGroupName(previousCollisionGroupId)
+    if not previousCollisionGroupName then return end
+
+    PhysicsService:SetPartCollisionGroup(object, previousCollisionGroupName)
+    previousCollisionGroups[object] = nil
+end
+
+local function onCharacterAdded(character)
+    setCollisionGroupRecursive(character)
+
+    character.DescendantAdded:Connect(setCollisionGroup)
+    character.DescendantRemoving:Connect(resetCollisionGroup)
+end
+
+local function onPlayerAdded(player)
+    player.CharacterAdded:Connect(onCharacterAdded)
+end
+
+Players.PlayerAdded:Connect(onPlayerAdded)
