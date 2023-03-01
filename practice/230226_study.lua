b0626ac075cbd7dc9cba7ec983a9ac65fe139f79
jey25 <jjangeoyoung@gmail.com>
2023-03-01 22:59:05 +0900

    Update 230226_study.lua

--- practice/230226_study.lua
+++ practice/230226_study.lua
@@ -1,2 +1,7 @@
 print("Start")
 print("I complexibity in my mind")
+
+print(workspace.Car.Color111.script)
+print(workspace.1x1 Curve Boulevard)
+print(workspace["1x1 Curve Boulevard"])
+print(workspace["한국어"])
