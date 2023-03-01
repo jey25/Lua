ddbc275f76d7c102e930adeaeb9420de4dc5a685
jey25 <jjangeoyoung@gmail.com>
2023-03-01 23:28:07 +0900

    Update 230226_study.lua

--- practice/230226_study.lua
+++ practice/230226_study.lua
@@ -5,3 +5,7 @@ print(workspace.Car.Color111.script)
 print(workspace.1x1 Curve Boulevard)
 print(workspace["1x1 Curve Boulevard"])
 print(workspace["한국어"])
+
+script.Parent.Transparency = 0.5
+script.Parent.Anchored = false
+script.Parent.Material = Enum.Material.Brick
