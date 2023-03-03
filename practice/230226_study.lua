42ba5c543e132c336000167087e821912a1486d6
jey25 <jjangeoyoung@gmail.com>
2023-03-04 00:00:42 +0900

    Update 230226_study.lua

--- practice/230226_study.lua
+++ practice/230226_study.lua
@@ -10,6 +10,7 @@ script.Parent.Transparency = 0.5
 script.Parent.Anchored = false
 script.Parent.Material = Enum.Material.Brick
 script.Parent.BackSurface = Enum.SurfaceType.Smooth
+
 workspace.Cube.BrickColor = BrickColor.new("New Yeller")
 workspace.Cube.BrickColor = BrickColor.Random()
 workspace.Cube.BrickColor = BrickColor.red()
