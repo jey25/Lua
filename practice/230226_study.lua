8a3864a71f5306aa55aecc2b4c3e5a2b848603be
jey25 <jjangeoyoung@gmail.com>
2023-03-06 00:00:38 +0900

    Update 230226_study.lua

--- practice/230226_study.lua
+++ practice/230226_study.lua
@@ -15,3 +15,23 @@ script.Parent.BackSurface = Enum.SurfaceType.Smooth
 workspace.Cube.BrickColor = BrickColor.new("New Yeller")
 workspace.Cube.BrickColor = BrickColor.Random()
 workspace.Cube.BrickColor = BrickColor.red()
+
+print(workspace.Car.PrimaryPart)
+workspace.Car.PrimaryPart = nil
+
+if workspace.Car.PrimaryPart then
+ if workspace.Car.PrimaryPart.Anchored then
+  print("aaaaaaaaaaaaa")
+ end
+end
+if workspace.Car.PrimaryPart and workspace.Car.PrimaryPart.Anchored  then
+ print("aaaaaaaaaaaaa")
+end
+
+if workspace.Car.PrimaryPart or workspace.Car.PrimaryPart.Anchored  then
+ print("aaaaaaaaaaaaa")
+end
+
+if true and (true or true) then
+ print("aaaaaaaaaaaaa")
+end
