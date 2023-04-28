c6ee749ce6eb5dad367fb9c2dd852e07ade164dd
Jang eo young <jjangeoyoung@gmail.com>
2023-04-28 16:25:36 +0900

    Update 230424_Review.lua

--- practice/230424_Review.lua
+++ practice/230424_Review.lua
@@ -3,22 +3,27 @@
 local ball1 = game.ServerStorage:FindFirstChild("rollingStone1")
 local ball2 = game.ServerStorage:FindFirstChild("rollingStone2")
 local ball3 = game.ServerStorage:FindFirstChild("rollingStone3")
+
 local balls = {ball1, ball2, ball3}
-local destroyHeight = 20
+
+local destroyHeight = 30
+
 
 while true do
 	for i=1, 3 do	
 		balls[i].Parent = workspace
-		balls[i].Position = Vector3.new(-150, 80, 363)
-		wait(1)
+		balls[i].Position = Vector3.new(-150, 90, 363)
+		wait(1.5)
 		balls[i].Parent = workspace
-		balls[i].Position = Vector3.new(-170, 80, 363)
-		wait(1)
+		balls[i].Position = Vector3.new(-170, 90, 363)
+		wait(1.5)
 		balls[i].Parent = workspace
-		balls[i].Position = Vector3.new(-183, 80, 363)
-		if balls[i].Position.Y <= destroyHeight then -- Y값이 파괴 높이 이하라면
+		balls[i].Position = Vector3.new(-183, 90, 363)
+		
+		if balls[i].Position.Y <= destroyHeight then -- Y값이 일정 높이 이하일 경우
 			balls[i]:Destroy() -- Part를 파괴합니다.
 		end
+		wait(.5)
 	end
 end
 
