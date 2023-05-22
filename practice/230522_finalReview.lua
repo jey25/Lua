3979cfeba39bca74554e12a047bfce694f9f4aa2
jey25 <jjangeoyoung@gmail.com>
2023-05-22 22:29:17 +0900

    Update 230522_finalReview.lua

--- practice/230522_finalReview.lua
+++ practice/230522_finalReview.lua
@@ -9,3 +9,49 @@ part.Touched:Connect(function(hit)
 	end
 end)
 
+
+--플레이어의 레벨을 리더보드에 세팅한다
+local function playerJoin(player)
+	local leaderstats = Instance.new("Folder")
+	leaderstats.Name = "leaderstats"
+	leaderstats.Parent = player
+	
+	local level = Instance.new("IntValue")
+	level.Name = "level"
+	level.Value = 1
+	level.Parent = leaderstats
+	
+end
+
+game.Players.PlayerAdded:Connect(playerJoin)
+
+
+--파트에 닿은 플레이어의 level 을 올려준다
+local part = script.Parent
+
+local function onTouched(hit)
+	local player = game.Players:GetPlayerFromCharacter(hit.Parent)
+	
+	if player then
+		player.leaderstats.level.Value += 1
+	end
+end
+
+part.Touched:Connect(onTouched)
+
+
+--파트에 닿은 플레이어의 레벨이 5 미만일때만 체력을 0으로 만든다
+local part = script.Parent
+
+local function onTouched(hit)
+	local humanoid = hit.Parent:FindFirstChild("Humanoid")
+	local player = game.Players:GetPlayerFromCharacter(hit.Parent)
+	
+	if player then
+		if humanoid and player.leaderstats.level.Value < 5 then
+			humanoid.Health = 0
+	end
+	end
+	end
+
+part.Touched:Connect(onTouched)
