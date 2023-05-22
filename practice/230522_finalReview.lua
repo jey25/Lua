16cb4c7da877a2b6b31589b2dad0ef841ba64250
Jang eo young <jjangeoyoung@gmail.com>
2023-05-22 16:23:21 +0900

    Update 230522_finalReview.lua

--- practice/230522_finalReview.lua
+++ practice/230522_finalReview.lua
@@ -1,1 +1,11 @@
 
+--캐릭터가 특정 part 에 도달했을 때 SpawnLocation 을 옮겨준다
+local part = script.Parent
+
+part.Touched:Connect(function(hit)
+	local humanoid = hit.Parent:FindFirstChild("Humanoid")
+	if humanoid then
+		workspace.SpawnLocation.Position = Vector3.new(-16, 6.5, 18.5)
+	end
+end)
+
