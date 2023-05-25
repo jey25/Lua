588589391eae194c1b1cd842b2cf8924ca5858f3
Jang eo young <jjangeoyoung@gmail.com>
2023-05-25 11:13:14 +0900

    Update MapleStoryWorld.lua

--- practice/MapleStoryWorld.lua
+++ practice/MapleStoryWorld.lua
@@ -1,6 +1,17 @@
-
 [server only]
 void OnBeginPlay()
 {
     log("Hello Maple World")
 }
+
+
+void CallPlaySoundEffect()
+{
+if isClient == true then
+--PlaySoundEffect를 실행하라!
+ 
+elseif isServer == true then
+--클라이언트의 PlaySoundEffect를 실행하라!
+end
+}
+
