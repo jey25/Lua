d12a96683355dd660ae09abe6b39a524326aa524
Jang eo young <jjangeoyoung@gmail.com>
2023-05-25 11:16:57 +0900

    Update MapleStoryWorld.lua

--- practice/MapleStoryWorld.lua
+++ practice/MapleStoryWorld.lua
@@ -15,3 +15,14 @@ elseif isServer == true then
 end
 }
 
+
+[ServerOnly]
+void CallPlaySoundEffect()
+{
+self:PlaySoundEffect()  -- 어떤 공간에서든 같은 공간의 함수를 호출하듯이 사용할 수 있다.
+}
+[client] --각 함수별로 어떤 공간에서 호출하고 실행할지를 설정할 수 있다.
+void PlaySoundEffect()
+{
+--코드 작성 공간입니다.
+}
