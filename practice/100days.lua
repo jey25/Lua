64d2efd6f2f5fb4b0a71805050dc428543f4f7a2
jey25 <jjangeoyoung@gmail.com>
2023-06-04 22:10:56 +0900

    Create 100days.lua

---
+++ practice/100days.lua
@@ -0,0 +1,14 @@
+Day1
+
+You are brave.
+Your life was not easy.
+In fact, no one gets to live an easy life, no matter how flawless it seems from the outside
+You are not an exception
+This means that you are standing here today, because you've made it through those challenging times
+You've been a victim, a survivor, and a hero.
+You didn't run away or give up on your life.
+You stayed alive and showed up.
+Now you are standing in the arena that you chose to be in, going all in, giving it all you've got.
+You are already brave.
+You are already a hero.
+That's who you are.
