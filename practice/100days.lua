002ea5a32b9cef9d8d9f712a588ca441cffc133a
jey25 <jjangeoyoung@gmail.com>
2023-06-07 22:50:34 +0900

    Update 100days.lua

--- practice/100days.lua
+++ practice/100days.lua
@@ -28,3 +28,20 @@ AS long as you are doing what's best for you, fear will always be there.
 So instead, let's say. "Come on fear. You are coming along."
 
 day3
+
+Do what scared you.
+Do you want to know how to overcome your fears? Just do it scared.
+What is the thing that you are most afraid to say or do?
+I challenge you to do it today.
+This is how you live your life to the fullest.
+If you have something that feels true to your heart, iet it out.
+Being scared should not be the sign to stop.
+In fact, the bigger your goal is, the more you are supposed to feel scared.
+If you feel totally comfortable with your dreams, set higher golas.
+Speak your truth. Do what you're meant to do.
+Be willing to feel scared.
+Then, you will not be scared of that anymore.
+You'll find another thing that makes you scared.
+The more you repeat this process, the more you can create an amazing life.
+
+    
