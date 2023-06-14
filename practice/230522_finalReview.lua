165f5806d516f85f1a912320065eaa238d7ec332
Jang eo young <jjangeoyoung@gmail.com>
2023-06-14 13:36:22 +0900

    Update 230522_finalReview.lua

--- practice/230522_finalReview.lua
+++ practice/230522_finalReview.lua
@@ -214,3 +214,26 @@ local function killPlayer(otherPart)
 end
 
 trapObject.Touched:Connect(killPlayer)
+
+
+--players 예시
+local Players = game:GetService("Players")
+
+local function onCharacterAdded(character)
+	-- Give them sparkles on their head if they don't have them yet
+	if not character:FindFirstChild("Sparkles") then
+		local sparkles = Instance.new("Sparkles")
+		sparkles.Parent = character:WaitForChild("Head")
+	end
+end
+
+local function onPlayerAdded(player)
+	-- Check if they already spawned in
+	if player.Character then
+		onCharacterAdded(player.Character)
+	end
+	-- Listen for the player (re)spawning
+	player.CharacterAdded:Connect(onCharacterAdded)
+end
+
+Players.PlayerAdded:Connect(onPlayerAdded)
