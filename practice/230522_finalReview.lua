e1e481284880898eb5f9e091f0e6e48fa7214c1a
jey25 <jjangeoyoung@gmail.com>
2023-06-29 23:11:44 +0900

    Update 230522_finalReview.lua

--- practice/230522_finalReview.lua
+++ practice/230522_finalReview.lua
@@ -263,3 +263,11 @@ script.Parent.BrickColor = BrickColor.Random()
 8. workspace.Part.Size = Vector3.new(10,5,20)
 
 9. Script 에서의 for 반복문 출력 (Print)
+
+10. workspace.Part.Position = workspace.Part.Position + Vector3.new(1, 0, 0)
+
+11. 파트 생성
+for i=1, 50 do
+	Instance.new("Part", workspace)
+	wait()
+end
