8343450e73225f91e81a5e1eac7286f933de6775
jey25 <jjangeoyoung@gmail.com>
2023-08-10 22:09:07 +0900

    Update 230730_FF_Review.lua

--- practice/230730_FF_Review.lua
+++ practice/230730_FF_Review.lua
@@ -1,6 +1,5 @@
 
 -- module script 기초
-
 local module = {}
 
 local runService = game:GetService("RunService")
@@ -30,6 +29,7 @@ local KillPartHandler = require(workspace.KillPartHandler)
 script.Parent.Touched:Connect(function(hit)
 	KillPartHandler.KillCharacterFromPart(hit)
 end)
+
 
 
 -- 모듈 스크립트는 서버, 클라이언트 각각 따로 돌아간다
