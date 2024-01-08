3ebfc4e0d0aa68c0815dbb8ffe21436c05aa7e44
Jang eo young <jjangeoyoung@gmail.com>
2024-01-08 17:32:50 +0900

    Update 231123_restartReview.lua

--- practice/231123_restartReview.lua
+++ practice/231123_restartReview.lua
@@ -157,3 +157,123 @@ local n = math.random(10, 20)/10
 if n <= 1.4 then
 	print("40%")
 end
+
+
+
+
+-- 쉬프트 달리가 + 카메라 쉐이크
+
+Bobbing Camera 스크립트 (스타터 GUI에 넣으세요)
+-------------------------------------------------------------------------------------
+while true do
+	wait();
+	if game.Players.LocalPlayer.Character then
+		break;
+	end;
+end;
+camera = game.Workspace.CurrentCamera;
+character = game.Players.LocalPlayer.Character;
+Z = 0;
+damping = character.Humanoid.WalkSpeed / 2;
+PI = 3.1415926;
+tick = PI / 2;
+running = false;
+strafing = false;
+character.Humanoid.Strafing:connect(function(p1)
+	strafing = p1;
+end);
+character.Humanoid.Jumping:connect(function()
+	running = false;
+end);
+character.Humanoid.Swimming:connect(function()
+	running = false;
+end);
+character.Humanoid.Running:connect(function(p2)
+	if p2 > 0.1 then
+		running = true;
+		return;
+	end;
+	running = false;
+end);
+function mix(p3, p4, p5)
+	return p4 + (p3 - p4) * p5;
+end;
+while true do
+	game:GetService("RunService").RenderStepped:wait();
+	fps = (camera.CoordinateFrame.p - character.Head.Position).Magnitude;
+	if fps < 0.52 then
+		Z = 1;
+	else
+		Z = 0;
+	end;
+	if running == true and strafing == false then
+		tick = tick + character.Humanoid.WalkSpeed / 92;
+	else
+		if tick > 0 and tick < PI / 2 then
+			tick = mix(tick, PI / 2, 0.9);
+		end;
+		if PI / 2 < tick and tick < PI then
+			tick = mix(tick, PI / 2, 0.9);
+		end;
+		if PI < tick and tick < PI * 1.5 then
+			tick = mix(tick, PI * 1.5, 0.9);
+		end;
+		if PI * 1.5 < tick and tick < PI * 2 then
+			tick = mix(tick, PI * 1.5, 0.9);
+		end;
+	end;
+	if PI * 2 <= tick then
+		tick = 0;
+	end;
+	camera.CoordinateFrame = camera.CoordinateFrame * CFrame.new(math.cos(tick) / damping, math.sin(tick * 2) / (damping * 2), Z) * CFrame.Angles(0, 0, math.sin(tick - PI * 1.5) / (damping * 20));
+end;
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+SprintScript 스크립트 (스타터GUI에 넣으세요)
+-----------------------------------------------------------------------------
+local Player = game.Players.LocalPlayer
+local Character = workspace:WaitForChild(Player.Name)
+local Humanoid = Character:WaitForChild('Humanoid')
+
+local RunAnimation = Instance.new('Animation')
+RunAnimation.AnimationId = 'rbxassetid://12961464334'
+RAnimation = Humanoid:LoadAnimation(RunAnimation)
+
+Running = false
+
+function Handler(BindName, InputState)
+	if InputState == Enum.UserInputState.Begin and BindName == 'RunBind' then
+		Running = true
+		Humanoid.WalkSpeed = 50
+	elseif InputState == Enum.UserInputState.End and BindName == 'RunBind' then
+		Running = false
+		if RAnimation.IsPlaying then
+			RAnimation:Stop()
+		end
+		Humanoid.WalkSpeed = 16
+	end
+end
+
+Humanoid.Running:connect(function(Speed)
+	if Speed >= 10 and Running and not RAnimation.IsPlaying then
+		RAnimation:Play()
+		Humanoid.WalkSpeed = 30
+	elseif Speed >= 10 and not Running and RAnimation.IsPlaying then
+		RAnimation:Stop()
+		Humanoid.WalkSpeed = 16
+	elseif Speed < 10 and RAnimation.IsPlaying then
+		RAnimation:Stop()
+		Humanoid.WalkSpeed = 16
+	end
+end)
+
+Humanoid.Changed:connect(function()
+	if Humanoid.Jump and RAnimation.IsPlaying then
+		RAnimation:Stop()
+	end
+end)
+
+game:GetService('ContextActionService'):BindAction('RunBind', Handler, true, Enum.KeyCode.LeftShift)
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+애니메이션 링크
+-------------------------------
+rbxassetid://1296146433
