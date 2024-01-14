09262938895082f59f48986821ae8387f5b401d4
jey25 <jjangeoyoung@gmail.com>
2024-01-14 21:20:56 +0900

    Update main.lua

--- udemy/TopDownShooter/main.lua
+++ udemy/TopDownShooter/main.lua
@@ -13,7 +13,8 @@ function love.load(...)
     player.y = love.graphics.getHeight()/2
     player.speed = 180
 
-    tempRotation = 0
+    zombies = {}
+
 end
 
 function love.update(dt)
@@ -30,16 +31,34 @@ function love.update(dt)
         player.y = player.y + player.speed* dt
     end
 
-    tempRotation = tempRotation + 0.01
-
 end
 
 
 function love.draw()
     love.graphics.draw(sprites.background, 0, 0)
     love.graphics.draw(sprites.player, player.x, player.y, playerMouseAngle(), nil, nil, sprites.player:getWidth()/2, sprites.player:getHeight()/2)
+
+    for i , z in ipairs(zombies) do
+        love.graphics.draw(sprites.zombie, z.x, z.y)
+    end
+end
+
+function love.keypressed(key)
+    if key  == "space" then
+        spawnZombie()
+    end
 end
 
 function playerMouseAngle()
     return math.atan2(player.y - love.mouse.getY(), player.x - love.mouse.getX()) + math.pi
 end
+
+function spawnZombie( ... )
+    local zombie = {}
+    zombie.x = math.random(0, love.graphics:getWidth())
+    zombie.y = math.random(0, love.graphics:getHeight())
+    zombie.speed = 100
+    table.insert(zombies, zombie)
+end
+
+
