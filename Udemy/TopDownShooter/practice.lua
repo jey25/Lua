4c28d358181f860fa3f1fc0c522ff96499014892
jey25 <jjangeoyoung@gmail.com>
2024-01-14 22:39:49 +0900

    Create practice.lua

---
+++ Udemy/TopDownShooter/practice.lua
@@ -0,0 +1,11 @@
+school = {name = nil, id= nil, "grade", isHigh = "No", locale = "seoul", "gildong"}
+
+name = "elli"
+id = 4
+
+school.name = name
+school.id = id
+
+print(school.name)
+print(school.id)
+print(school.locale)
