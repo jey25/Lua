55a331a44fa3ee1453664cfd3ffa1b3179eace6c
Jang eo young <jjangeoyoung@gmail.com>
2024-01-18 11:25:23 +0900

    Update main.lua

--- udemy/TopDownShooter/main.lua
+++ udemy/TopDownShooter/main.lua
@@ -48,6 +48,35 @@ function love.update(dt)
         b.y = b.y + (math.sin( b.direction ) * b.speed * dt)
     end
 
+    for i=#bullets, 1, -1 do
+        local b = bullets[i]
+        if b.x < 0 or b.y < 0 or b.x > love.graphics.getWidth() or b.y > love.graphics.getHeight() then
+            table.remove( bullets, i )
+        end
+    end
+
+    for i,z in ipairs(zombies) do
+        for j,b in ipairs(bullets) do
+            if distanceBetween(z.x, z.y, b.x, b.y) < 20 then
+                z.dead = true
+                b.dead = true
+            end
+        end
+    end
+
+    for i=#zombies, 1, -1 do
+        local z = zombies[i]
+        if z.dead == true then
+            table.remove(zombies, i)
+        end
+    end
+
+    for i=#bullets, 1, -1 do
+        local b = bullets[i]
+        if b.dead == true then
+            table.remove(bullets, i)
+        end
+    end
 end
 
 
@@ -57,10 +86,11 @@ function love.draw()
 
     for i , z in ipairs(zombies) do
         love.graphics.draw(sprites.zombie, z.x, z.y, zombiePlayerAngle(z), nil, nil, sprites.zombie:getWidth()/2, sprites.zombie:getHeight()/2)
+        
     end
 
     for i , b in ipairs(bullets) do
-        love.graphics.draw(sprites.bullet, b.x, b.y)
+        love.graphics.draw(sprites.bullet, b.x, b.y, nil, 0.3, nil,  sprites.bullet:getWidth()/2, sprites.bullet:getHeight()/2)
     end
 
 end
@@ -91,6 +121,7 @@ function spawnZombie( ... )
     zombie.x = math.random(0, love.graphics:getWidth())
     zombie.y = math.random(0, love.graphics:getHeight())
     zombie.speed = 140
+    zombie.dead = false
     table.insert(zombies, zombie)
 end
 
@@ -98,7 +129,8 @@ function spawnBullet( ... )
     local bullet = {}
     bullet.x = player.x
     bullet.y = player.y
-    bullet.speed = 500
+    bullet.speed = 700
+    bullet.dead = false
     bullet.direction = playerMouseAngle()
     table.insert( bullets, bullet)
 end
