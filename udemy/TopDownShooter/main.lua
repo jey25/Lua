0896b321a264d5603d000364c1aee58faf192995
Jang eo young <jjangeoyoung@gmail.com>
2024-01-18 11:41:23 +0900

    Update main.lua

--- udemy/TopDownShooter/main.lua
+++ udemy/TopDownShooter/main.lua
@@ -118,11 +118,27 @@ end
 
 function spawnZombie( ... )
     local zombie = {}
-    zombie.x = math.random(0, love.graphics:getWidth())
-    zombie.y = math.random(0, love.graphics:getHeight())
+    zombie.x = 0
+    zombie.y = 0
     zombie.speed = 140
     zombie.dead = false
     table.insert(zombies, zombie)
+
+    local side = math.random(1, 4)
+
+    if side == 1 then
+        zombie.x = -30
+        zombie.y = math.random(0, love.graphics.getHeight())
+    elseif side == 2 then
+        zombie.x = love.graphics.getWidth() + 30
+        zombie.y = math.random(0, love.graphics.getHeight())
+    elseif side == 3 then
+        zombie.x = math.random(0, love.graphics.getWidth())
+        zombie.y = -30
+    elseif side == 4 then
+        zombie.x = math.random(0, love.graphics.getWidth())
+        zombie.y = love.graphics.getHeight() + 30
+    end
 end
 
 function spawnBullet( ... )
