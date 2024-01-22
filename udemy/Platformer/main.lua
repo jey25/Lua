e95f137feb0a81238acba028c3c1abbc23099f5d
Jang eo young <jjangeoyoung@gmail.com>
2024-01-22 11:21:43 +0900

    Update main.lua

--- udemy/Platformer/main.lua
+++ udemy/Platformer/main.lua
@@ -1,28 +1,40 @@
 
 function love.load( ... )
     wf = require("libraries/windfield") 
-    world = wf.newWorld(0, 800)
+    world = wf.newWorld(0, 800, false)
 
-    world:addCollisionClass('platform')
-    world:addCollisionClass('player', {ignores = 'platform'})
+    world:addCollisionClass('Platform')
+    world:addCollisionClass('Player' --[[{ignores = {'Platform'}}]])
+    world:addCollisionClass('Danger')
 
-    player = world:newRectangleCollider(360, 100, 80, 80, {collision_class = 'player'})
+    player = world:newRectangleCollider(360, 100, 80, 80, {collision_class = 'Player'})
+    player:setFixedRotation(true)
     player.speed = 200
 
-    platform = world:newRectangleCollider(250, 400, 300, 100, {collision_class = 'platform'})
+    platform = world:newRectangleCollider(250, 400, 300, 100, {collision_class = 'Platform'})
     platform:setType('static')
+
+    dangerZone = world:newRectangleCollider(0, 550, 800, 50, {collision_class = 'Danger'})
+    dangerZone:setType('static')
+
 end
 
 
 function love.update(dt)
     world:update(dt)
 
-    local px, py = player:getPosition()
-    if love.keyboard.isDown('right') then
-        player:setX(px + player.speed* dt)
-    end
-    if love.keyboard.isDown('left') then
-        player:setX(px - player.speed* dt)
+    if player.body then
+        local px, py = player:getPosition()
+        if love.keyboard.isDown('right') then
+            player:setX(px + player.speed* dt)
+        end
+        if love.keyboard.isDown('left') then
+            player:setX(px - player.speed* dt)
+        end
+
+        if player:enter('Danger') then
+            player:destroy()
+        end
     end
 end
 
