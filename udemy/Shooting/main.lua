b23580a04d0a9e91cdd11b9da41b3b8d4de4477e
Jang eo young <jjangeoyoung@gmail.com>
2024-03-21 16:44:51 +0900

    Update main.lua

--- udemy/Shooting/main.lua
+++ udemy/Shooting/main.lua
@@ -30,14 +30,14 @@ function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.setColor(1,0,0)
-    love.graphics.circle("fill", target.x, target.y, target.radius)
+    love.graphics.draw(sprites.sky, 0, 0)
     
     love.graphics.setColor(1,1,1)
     love.graphics.setFont(gameFont)
     love.graphics.print(score, 0, 0)
     love.graphics.print(math.ceil(timer), 300, 0)
 
+    love.graphics.draw(sprites.target, target.x - target.radius, target.y - target.radius)
     love.graphics.draw(sprites.crosshairs, love.mouse.getX()-20, love.mouse.getY()-20)
 
 end 
