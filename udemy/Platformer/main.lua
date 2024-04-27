5f2b91c77f89b00e4e523c701824db749944aba3
Jang eo young <jjangeoyoung@gmail.com>
2024-04-27 10:30:43 +0900

    update animations

--- udemy/Platformer/main.lua
+++ udemy/Platformer/main.lua
@@ -20,10 +20,11 @@ function love.load( ... )
     world:addCollisionClass('Player' --[[{ignores = {'Platform'}}]])
     world:addCollisionClass('Danger')
 
-    player = world:newRectangleCollider(360, 100, 80, 80, {collision_class = 'Player'})
+    player = world:newRectangleCollider(360, 100, 40, 100, {collision_class = 'Player'})
     player:setFixedRotation(true)
     player.speed = 200
-    player.animation = animations.run
+    player.animation = animations.idle
+    player.isMoving = false
 
     platform = world:newRectangleCollider(250, 400, 300, 100, {collision_class = 'Platform'})
     platform:setType('static')
@@ -38,12 +39,15 @@ function love.update(dt)
     world:update(dt)
 
     if player.body then
+        player.isMoving = false
         local px, py = player:getPosition()
         if love.keyboard.isDown('right') then
             player:setX(px + player.speed* dt)
+            player.isMoving = true
         end
         if love.keyboard.isDown('left') then
             player:setX(px - player.speed* dt)
+            player.isMoving = true
         end
 
         if player:enter('Danger') then
@@ -51,20 +55,28 @@ function love.update(dt)
         end
     end
 
+    if player.isMoving == true then
+        player.animation = animations.run
+    else
+        player.animation = animation.idle
+    end
+
     player.animation:update(dt)
 end
 
 
-function love.draw( ... )
+function love.draw()
     world:draw()
-    player.animation:draw(sprites.playerSheet, 0, 0)
+
+    local px, py = player:getPosition()
+    player.animation:draw(sprites.playerSheet, px, py, nil, 0.25, nil, 130, 300)
 end
 
 function love.keypressed( key )
     if key == 'up' then
-        local colliders = world:queryRectangleArea(player:getX() - 40, player:getY() + 40, 80, 2, {'Platform'})
+        local colliders = world:queryRectangleArea(player:getX() - 20, player:getY() + 50, 40, 2, {'Platform'})
         if #colliders > 0 then
-            player:applyLinearImpulse(0, -7000)
+            player:applyLinearImpulse(0, -4000)
         end
     end
 end
