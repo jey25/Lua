188f5e525623e9851630e93b53706e75350f32b2
Jang eo young <jjangeoyoung@gmail.com>
2024-10-10 16:23:51 +0900

    Update main.lua

--- udemy/241010_Re/main.lua
+++ udemy/241010_Re/main.lua
@@ -1,4 +1,9 @@
+message = 5
+message2 = 10
+output = message + message2
+
 function love.draw()
     love.graphics.setFont(love.graphics.newFont(60))
-    love.graphics.print("Hello")
+    love.graphics.print(output)
+    love.graphics.print(type(output))
 end
