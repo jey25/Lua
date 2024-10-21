6ffdcc89aa164051ad401a016d0fd12ad5b400f1
Jang eo young <jjangeoyoung@gmail.com>
2024-10-21 16:42:35 +0900

    Create enemy.lua

---
+++ udemy/Platformer/enemy.lua
@@ -0,0 +1,19 @@
+
+enemies = {}
+
+function spawnEnemy(x, y)
+    local enemy = world:newRectangleCollider(x, y, 70, 90, {collision_class = "Danger"})
+    enemy.direction = 1
+    enemy.speed = 200
+    table.insert(enemies, enemy)
+end
+
+function updateEnemies(dt)
+    for i, e in ipairs(enemies) do
+        local ex, ey = e:getPosition()
+
+        local colliders = world:queryRectangleArea(ex + (40 * e.direction), ey + 40, 10, 10, {})
+
+        e:setX(ex + e.speed * dt * e.direction)
+    end
+end
