9d25cc4229c601aa0d60e083fea1afdf647098ad
Jang eo young <jjangeoyoung@gmail.com>
2024-10-21 17:30:41 +0900

    update platform

--- udemy/Platformer/main.lua
+++ udemy/Platformer/main.lua
@@ -15,14 +15,17 @@ function love.load( ... )
     -- 캐릭터 이미지 불러오기 및 Animation 세팅
     sprites = {}
     sprites.playerSheet = love.graphics.newImage('sprites/playerSheet.png')
+    sprites.enemySheet = love.graphics.newImage('sprites/enemySheet.png')
 
     -- 전체 이미지를 한장의 Grid 로 나누어 저장
     local grid = anim8.newGrid(614, 564, sprites.playerSheet:getWidth(), sprites.playerSheet:getHeight())
+    local enemyGird = anim8.newGrid(100, 79, sprites.enemySheet:getWidth(), sprites.enemySheet:getHeight())
 
     animations = {}
     animations.idle = anim8.newAnimation(grid('1-15', 1), 0.05)
     animations.jump = anim8.newAnimation(grid('1-7', 2), 0.05)
     animations.run = anim8.newAnimation(grid('1-15', 3), 0.05)
+    animations.enemy = anim8.newAnimation(enemyGird('1-2', 1), 0.03)
 
     world:setQueryDebugDrawing(true)
     world:addCollisionClass('Platform')
@@ -31,6 +34,7 @@ function love.load( ... )
 
     -- 동일 경로에 위치한 player.lua 파일을 require
     require('player')
+    require('enemy')
 
     dangerZone = world:newRectangleCollider(0, 550, 800, 50, {collision_class = 'Danger'})
     dangerZone:setType('static')
@@ -38,6 +42,7 @@ function love.load( ... )
     Platforms = {}
 
     loadMap()
+    spawnEnemy(900, 50)
 
 end
 
@@ -45,6 +50,7 @@ function love.update(dt)
     world:update(dt)
     gameMap:update(dt)
     PlayerUpdate(dt)
+    updateEnemies(dt)
 
     local px, py = player:getPosition()
     cam:lookAt(px, love.graphics.getHeight()/2)
@@ -53,8 +59,9 @@ end
 function love.draw()
     cam:attach()
         gameMap:drawLayer(gameMap.layers["Tile Layer 1"])
-        -- world:draw()
+        world:draw()
         DrawPlayer()
+        drawEnemies()
     cam:detach()
 end
 
